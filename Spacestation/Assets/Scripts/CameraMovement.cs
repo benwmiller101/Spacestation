0f90f4dcb5bab2397f37d872f1e577d05c192bce
benwmiller101 <1803930@rgu.ac.uk>
2020-06-07 20:22:41 +0100

    Camera Start

---
+++ Spacestation/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+
+    private Vector3 previousPosition;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(0)){
+            Vector3 direction = previousPosition - cam.ScreenToViewportPoint(Input.mousePosition);
+
+            cam.transform.position = new Vector3();
+
+            cam.transform.Rotate(new Vector3(1,0,0), direction.y * 180);
+            cam.transform.Rotate(new Vector3(0,1,0), -direction.x * 180, Space.World);
+            cam.transform.Translate(new Vector3(0,0,-10));
+
+            previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+    }
+}
