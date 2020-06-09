670c81f7a0209bfd02033b7d775790ac73a0e721
benwmiller101 <1803930@rgu.ac.uk>
2020-06-09 12:43:28 +0100

    Selection shader (Not working)
    The shader works, just want it to work so when you select different objects the shader changes to the object you have selected.

--- Spacestation/Assets/Scripts/AddModule.cs
+++ Spacestation/Assets/Scripts/AddModule.cs
@@ -12,7 +12,7 @@ public class AddModule : MonoBehaviour
     public GameObject CubeBlue;
     public GameObject Cross;
     public GameObject Long;
-    public GameObject hardpoint;
+    public GameObject node;
     public GameObject[] array = new GameObject[10];
 
 //Every frame, if mouse is clicking on hardpoing
@@ -23,19 +23,35 @@ public class AddModule : MonoBehaviour
             RaycastHit hit;
             
             if( Physics.Raycast( ray, out hit, 100 )) {
-                if(hit.transform.gameObject.tag == "hardpoint") {
+                if(hit.transform.gameObject.tag == "node") {
                     PlacementMenu.gameObject.SetActive(true);
-                    hardpoint = hit.transform.gameObject;
+                    node = hit.transform.gameObject;
+                }
+            }
+        }
+    }
+
+    private void NodeHover()
+    {
+        if (Input.GetMouseButtonDown(1)){
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.transform.gameObject.tag == "Module")
+                {
+                    //hit.transform.gameObject
                 }
             }
         }
     }
 
 
-//Shows all Hardpoints (gameObjects tagged with hp)
-    public void showHardpoint() {
+//Shows all nodes (gameObjects tagged with hp)
+    public void shownode() {
         GameObject[] hps ;
-        hps = GameObject.FindGameObjectsWithTag("hardpoint");
+        hps = GameObject.FindGameObjectsWithTag("node");
 
         foreach(GameObject SmallHP in hps) {
                 SmallHP.GetComponent<MeshRenderer>().enabled=true;
@@ -43,21 +59,21 @@ public class AddModule : MonoBehaviour
     }
 
 
-// Called when Button pressed (instantiates module at hardpoint), takes in string to decide type
+// Called when Button pressed (instantiates module at node), takes in string to decide type
     public void Instantiate(string type) {
         PlacementMenu.SetActive(false);
 
         if(type == "green"){
-            Instantiate(CubeGreen, hardpoint.transform.position, Quaternion.identity);
+            Instantiate(CubeGreen, node.transform.position, Quaternion.identity);
         }else if(type == "blue"){
-            Instantiate(CubeBlue, hardpoint.transform.position, Quaternion.identity);
+            Instantiate(CubeBlue, node.transform.position, Quaternion.identity);
         }else if(type == "long") {
-            Instantiate(Long, hardpoint.transform.position, Quaternion.identity);
+            Instantiate(Long, node.transform.position, Quaternion.identity);
         }else if(type == "cross") {
-            Instantiate(Cross, hardpoint.transform.position, Quaternion.identity);
+            Instantiate(Cross, node.transform.position, Quaternion.identity);
         }
         
-        hardpoint.SetActive(false);
+        node.SetActive(false);
     }
 
 }
---
+++ Spacestation/Assets/SelectModule.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA;
+
+public class SelectModule : MonoBehaviour
+{
+    public Material mat2;
+    public Material mat1;
+    private Transform LastSelected;
+
+    void Update()
+    {
+        Select();
+    }
+
+    private void Select()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.transform.gameObject.tag == "Module")
+                {
+                    if (LastSelected.transform != hit.transform)
+                    {
+                        LastSelected = hit.transform;
+                        Debug.Log("Hit");
+                        hit.transform.gameObject.GetComponent<Renderer>().material = mat1;
+                        LastSelected.gameObject.GetComponent<Renderer>().material = mat2;
+                    }
+                }
+            }
+        }
+    }
+}
