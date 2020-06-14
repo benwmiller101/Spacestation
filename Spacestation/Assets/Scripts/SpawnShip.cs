b42ba00625d1c5e9881f2fa80a210593bad30c4a
benwmiller101 <1803930@rgu.ac.uk>
2020-06-14 14:09:16 +0100

    Basic Dock

---
+++ Spacestation/Assets/Scripts/SpawnShip.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnShip : MonoBehaviour
+{
+
+    public GameObject ship;
+    public Animator anim;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            anim.Play("DockOpen");
+            Instantiate(ship, transform.position, Quaternion.identity);
+        } 
+    }
+}
