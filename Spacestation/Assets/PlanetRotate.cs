542cd016f90357405d918dd409d4dea0288699e5
benwmiller101 <1803930@rgu.ac.uk>
2020-06-16 22:45:31 +0100

    Planets

---
+++ Spacestation/Assets/PlanetRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotate : MonoBehaviour
+{
+    public float speed;
+    private Vector3 target;
+    public GameObject pivot;
+
+
+    void Start()
+    {
+        target = new Vector3(Random.Range(1, 30), Random.Range(1, 30), Random.Range(1, 30));
+    }
+    void Update()
+    {
+        transform.RotateAround(pivot.transform.position, target, speed * Time.deltaTime);
+    }
+}
---
+++ Spacestation/Assets/PointTowards.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointTowards : MonoBehaviour
+{
+    public GameObject pivot;
+
+    void Update()
+    {
+        transform.LookAt(pivot.transform.position);
+    }
+}
